4badd11d4d33f2ffd02ca614bd46ca13fc2f1672
softl <dongh_e@sina.com>
2016-01-31 21:10:58 +0800

    Delete mac.sh

--- mac.sh
+++
@@ -1,6 +0,0 @@
-mac="00:"`echo $RANDOM | md5sum | sed 's/\(..\)/&:/g' | cut -c1-14`
-echo $mac					
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 hw ether $mac
-sudo ifconfig wlan0 up
-sudo dhclient
